b27cded89f75c6da0998402feca065ee0989e3ed
Oguzkagan Findik <oguzfindik14@gmail.com>
2023-05-05 12:25:28 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Math.Pow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sonuc = UsAl(2, 3);
+            Console.WriteLine(sonuc);
+
+        }
+
+        public static int UsAl(int tabanSayi, int us)
+
+        {
+            int sonuc = 1;
+            for (int i = 0; i < us; i++)
+            {
+                sonuc *= tabanSayi;
+            }
+            return sonuc;
+        }
+
+    }
+}
+
